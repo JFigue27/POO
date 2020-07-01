878170d52a7db6d81a8e53590aa9958d03841950
jjfigueroa <josejaime.figueroa@molex.com>
2020-07-01 12:49:59 -0600

    Objects and classe in Phyton

--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,14 @@
+from car import Car
+
 if __name__ == "__main__":
     print("Hola Mundo!!")
+
+    car = Car()
+    car.license = "JJFIGUE123"
+    car.driver = "Jose Jaime"
+    print(vars(car))
+
+    car2 = Car()
+    car2.license = "JJFIG"
+    car2.driver = "Jaime Abraham"
+    print(vars(car2))
