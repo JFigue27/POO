932ae91d7f5fbd98d01d125d60b6cfd5fe0cf0c7
jjfigueroa <josejaime.figueroa@molex.com>
2020-07-01 12:41:45 -0600

    Using object class in CSharp

--- CSharp/Entities/car.cs
+++ CSharp/Entities/car.cs
@@ -1,11 +1,18 @@
+using System;
+
 namespace CSharp.Entities
 {
-    public class car
+    public class Car
     {
         public int id { get; set; }
         public string license { get; set; }
         public string driver { get; set; }
         public int passenger { get; set; }
+
+        public void printDataCar()
+        {
+            Console.WriteLine("License " + license + " Driver " + driver);
+        }
 
     }
 }
--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CSharp.Entities;
 
 namespace CSharp
 {
@@ -7,6 +8,13 @@ namespace CSharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var car = new Car();
+            car.id = 1;
+            car.driver = "Jaime Figueroa";
+            car.license = "JJFI123456";
+            car.passenger = 4;
+            car.printDataCar();
+
         }
     }
 }
